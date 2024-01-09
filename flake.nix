14af2e5d1279eb1cd3db34395487baffaaa3b600
Slava <visla.vvi@gmail.com>
2024-01-09 18:06:49 +0300

    Reproduce a fail with a mixed table in lualine config

---
+++ flake.nix
@@ -0,0 +1,39 @@
+# @copy-paste from https://github.com/nix-community/nixvim README.md
+{
+  description = "A very basic flake";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  inputs.nixvim.url = "github:nix-community/nixvim";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = {
+    self,
+    nixpkgs,
+    nixvim,
+    flake-utils,
+  }: let
+    # NOTE try to uncomment one of the config definitions
+
+    # This is fine
+    # config = { plugins.lualine.tabline.lualine_a = [ "tabs" ]; };
+
+    # But both of these fail
+    config = { plugins.lualine.tabline.lualine_a = { 0 = "tabs"; mode = 2 }; };
+    # config = { plugins.lualine.tabline.lualine_a = { "0" = "tabs"; mode = 2 }; };
+  in
+    flake-utils.lib.eachDefaultSystem (system: let
+      nixvim' = nixvim.legacyPackages."${system}";
+      nvim = nixvim'.makeNixvim config;
+    in {
+      packages = {
+        inherit nvim;
+        default = nvim;
+      };
+      devShells = {
+        myshell = nixpkgs.mkShell {
+          buildInputs = [nvim];
+          shellHook = "nvim";
+        };
+      };
+    });
+}
